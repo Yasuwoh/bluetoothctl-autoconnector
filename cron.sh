6666aafda9dafeba9903919499d97641e5b42dd3
Hasegawa Yasuwo <yasuwo.h@gmail.com>
2022-08-12 09:52:30 +0900

    cronで使うことを想定して余計なメッセージの出力を抑制

--- cron.sh
+++ cron.sh
@@ -3,13 +3,15 @@
 # WARNING: Changing this file will affect cron immediately
 
 fullpath=$(dirname $(realpath $0))
+$fullpath/autoconnector.sh -f $fullpath/list --quiet 2>&1
 
-(
-  echo "=================================== $(date) ==================================="
-  $fullpath/autoconnector.sh | sed -E 's/\xd$//g' # sed: replace the last "^M" with "\n"
-  echo "===================================================================================================="
-  echo
-) >> $fullpath/log/`date '+%Y-%m-%d'`.log 2>&1
 
-# create symlink of the latest log file as file name "latest.log"
-ln -sf $fullpath/log/`date '+%Y-%m-%d'`.log $fullpath/log/latest.log
+#(
+#  echo "=================================== $(date) ==================================="
+#  $fullpath/autoconnector.sh | sed -E 's/\xd$//g' # sed: replace the last "^M" with "\n"
+#  echo "===================================================================================================="
+#  echo
+#) >> $fullpath/log/`date '+%Y-%m-%d'`.log 2>&1
+#
+## create symlink of the latest log file as file name "latest.log"
+#ln -sf $fullpath/log/`date '+%Y-%m-%d'`.log $fullpath/log/latest.log
